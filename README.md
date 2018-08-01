5bbe83535b79245fa3498199031276ce467cacc7
dengtao <adt19930517@foxmail.com>
2018-08-01 08:32:07 +0800

    xiao jie jie

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # douyin-biaobai-xiaojiejie
 抖音表白小姐姐Python代码！
+
+需要安装pip install PyInstaller
+	   pip install PyQt5
+
+执行main.py中的main方法进行打包成exe文件！
+	   
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 """
-__title__ = '将calculator项目转换为exe文件'
-__author__ = '遂越净郝'
+__title__ = '将confession项目转换为exe文件'
+__author__ = 'dt'
 """
 from PyInstaller.__main__ import run
 
 if __name__ == '__main__':
-    # opts = ['confession.py', '-w', '--onefile']
+    opts = ['confession.py', '-w', '--onefile']
     #opts = ['confession.py', '-F']
-    opts = ['confession.py', '-F', '-w']
+    #opts = ['confession.py', '-F', '-w']
     #opts = ['confession.py', '-F', '-w', '--icon=TargetOpinionMain.ico','--upx-dir','upx391w']
     run(opts)
